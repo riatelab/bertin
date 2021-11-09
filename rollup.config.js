5bc2680d50b94e16b40d01e9d363277f905c0eaa
neocarto <nicolas.lambert@cnrs.fr>
2021-11-09 17:32:45 +0100

    test

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,36 @@
-// import de nos plugins
 import commonjs from '@rollup/plugin-commonjs';
 import noderesolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 
-export default {
-    input: 'src/index.js', // notre fichier source au format ESM
+const iife = {
+    input: 'src/index.js',
     output: {
         format: 'iife',
-        file: 'js/main.iife.min.js',
-        // les modules iife doivent être nommés afin de pouvoir y faire référence depuis d'autres modules
-        name: 'bertinjs'
+        file: 'dist/main.iife.min.js',
+        name: 'bertin'
     },
     plugins: [
-        commonjs(), // prise en charge de require
-        noderesolve(), // prise en charge des modules depuis node_modules
-        babel({ babelHelpers: 'bundled' }), // transpilation
-        terser() // minification
+        commonjs(),
+        noderesolve(),
+        babel({ babelHelpers: 'bundled' }),
+        terser()
     ]
 };
+
+const esm = {
+    input: 'src/index.js',
+    output: {
+        format: 'es',
+        file: 'dist/main.esm.min.js'
+    },
+    plugins: [
+        commonjs(),
+        noderesolve(),
+        babel({ babelHelpers: 'bundled' }),
+        terser()
+    ]
+};
+
+const conf = process.env.BABEL_ENV === 'esm' ? esm : iife;
+export default conf;
