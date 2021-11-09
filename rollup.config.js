86f45b8be099580fcd6cecbd53b0b632a2a8e1f0
neocarto <nicolas.lambert@cnrs.fr>
2021-11-09 17:03:50 +0100

    rollup config

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+// import de nos plugins
+import commonjs from '@rollup/plugin-commonjs';
+import noderesolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+    input: 'src/index.js', // notre fichier source au format ESM
+    output: {
+        format: 'iife',
+        file: 'js/main.iife.min.js',
+        // les modules iife doivent être nommés afin de pouvoir y faire référence depuis d'autres modules
+        name: 'bertin'
+    },
+    plugins: [
+        commonjs(), // prise en charge de require
+        noderesolve(), // prise en charge des modules depuis node_modules
+        babel({ babelHelpers: 'bundled' }), // transpilation
+        terser() // minification
+    ]
+};
