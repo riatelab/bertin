64312d32d01ba77f620fd2aa077af13a96bac96c
neocarto <nicolas.lambert@cnrs.fr>
2021-11-10 11:59:36 +0100

    fix dependencies

--- src/helpers/layer-simple.js
+++ src/helpers/layer-simple.js
@@ -1,3 +1,8 @@
+import * as d3selection from "d3-selection";
+import * as d3geo from "d3-geo";
+import * as d3geoprojection from "d3-geo-projection";
+const d3 = Object.assign({}, d3selection, d3geo, d3geoprojection);
+
 export function simpleLayer(selection, projection, features, options = {}) {
   let cols = [
     "#66c2a5",
