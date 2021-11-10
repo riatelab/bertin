7ba96176bacd3d8702d5ab0cb6ca121ee6b1ee26
neocarto <nicolas.lambert@cnrs.fr>
2021-11-10 12:47:46 +0100

    fix figuration

--- src/helpers/layer-simple.js
+++ src/helpers/layer-simple.js
@@ -1,4 +1,5 @@
 // import * as d3selection from "d3-selection";
+import * as d3selection from "d3-selection";
 import * as d3geo from "d3-geo";
 import * as d3geoprojection from "d3-geo-projection";
 const d3 = Object.assign({}, d3selection, d3geo, d3geoprojection);
