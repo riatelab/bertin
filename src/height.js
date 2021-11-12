1de074eca2dd064bd9dd12ab5276d234c76190cf
neocarto <nicolas.lambert@cnrs.fr>
2021-11-12 14:10:34 +0100

    plot function

--- src/height.js
+++ src/height.js
@@ -4,24 +4,33 @@ import * as d3geo from "d3-geo";
 import * as d3geoprojection from "d3-geo-projection";
 const d3 = Object.assign({}, d3selection, d3geo, d3geoprojection);
 
-// Height
-export function getheight(features, projection, width, outline) {
-  let ref;
-  let d;
+export function getheight(features, projection, width) {
 
-  if (outline) {
+  let ref;
+  let delta;
+
+  if (layers.find((d) => d.type == "outline") != undefined) {
+    let outline = layers.find((d) => d.type == "outline");
     ref = { type: "Sphere" };
-    d = outline.strokewidth ? outline.strokewidth : 0;
+    delta = outline.strokewidth ? outline.strokewidth : 0;
   } else {
-    ref = features;
-    d = 0;
+    let l = layers.map((d) => d.geojson).filter((d) => d !== undefined);
+    let all = [];
+    l.forEach((d) => all.push(d.features));
+    ref = {
+      type: "FeatureCollection",
+      features: all.flat()
+    };
+    delta = 1;
   }
   const [[x0, y0], [x1, y1]] = d3
     .geoPath(projection.fitWidth(width, ref))
     .bounds(ref);
   const dy = Math.ceil(y1 - y0),
     l = Math.min(Math.ceil(x1 - x0), dy);
-  projection.scale((projection.scale() * (l - d)) / l).precision(0.2);
+  projection.scale((projection.scale() * (l - delta)) / l).precision(0.2);
   let height = dy;
+  //let height = dy + header.fontsize + footer.fontsize;
   return height;
+  //return ref;
 }
