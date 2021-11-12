db396820e5fa9ce30c82c1d80ea89651758d5554
neocarto <nicolas.lambert@cnrs.fr>
2021-11-12 13:52:22 +0100

    plot function

--- src/plot.js
+++ src/plot.js
@@ -15,120 +15,122 @@ import { figuration } from "./figuration.js";
 //import { plotHeader, plotFooter, plotGraticule, plotOutline, getHeight} from "./helpers/layout.js";
 
 export function plot({ params = {}, layers = {} } = {}) {
-  // default global paramaters
 
-  let projection = params.projection ? params.projection : d3.geoPatterson();
-  let width = params.width ? params.width : 1000;
+  function plot({ params = {}, layers = {} } = {}) {
+    // default global paramaters
 
-  // optimal heights
-  let height = getheight(layers, projection, width);
-  let heightHeader = 0;
-  let header = layers.find((d) => d.type == "header");
-  if (header) {
-    if (header.text) {
-      heightHeader = 20;
+    let projection = params.projection ? params.projection : d3.geoPatterson();
+    let width = params.width ? params.width : 1000;
+
+    // optimal heights
+    let height = getheight(layers, projection, width);
+    let heightHeader = 0;
+    let header = layers.find((d) => d.type == "header");
+    if (header) {
+      if (header.text) {
+        heightHeader = 20;
+      }
+      if (header.fontsize) {
+        heightHeader = header.fontsize;
+      }
     }
-    if (header.fontsize) {
-      heightHeader = header.fontsize;
+    let heightFooter = 0;
+    let footer = layers.find((d) => d.type == "footer");
+    if (footer) {
+      if (footer.text) {
+        heightFooter = 20;
+      }
+      if (footer.fontsize) {
+        heightFooter = footer.fontsize;
+      }
     }
-  }
-  let heightFooter = 0;
-  let footer = layers.find((d) => d.type == "footer");
-  if (footer) {
-    if (footer.text) {
-      heightFooter = 20;
-    }
-    if (footer.fontsize) {
-      heightFooter = footer.fontsize;
-    }
-  }
 
-  // svg document
-  const svg = d3
-    .create("svg")
-    .attr("width", width)
-    .attr("height", height + heightHeader + heightFooter)
-    .attr("viewBox", [
-      0,
-      -heightHeader,
-      width,
-      height + heightHeader + heightFooter
-    ])
-    .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+    // svg document
+    const svg = d3
+      .create("svg")
+      .attr("width", width)
+      .attr("height", height + heightHeader + heightFooter)
+      .attr("viewBox", [
+        0,
+        -heightHeader,
+        width,
+        height + heightHeader + heightFooter
+      ])
+      .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
 
-  // clipPath
-  svg
-    .append("clipPath")
-    .attr("id", "clip")
-    .append("rect")
-    .attr("x", 0)
-    .attr("y", 0)
-    .attr("width", width)
-    .attr("height", height);
+    // clipPath
+    svg
+      .append("clipPath")
+      .attr("id", "clip")
+      .append("rect")
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", width)
+      .attr("height", height);
 
-  // Outline (fill)
-  let outline = layers.find((d) => d.type == "outline");
-  if (outline) {
-    addoutline(svg, projection, {
-      fill: outline.fill,
-      stroke: "none",
-      strokewidth: "none"
-    });
-  }
-
-  // ----------------------------------------
-  layers.forEach((layer) => {
-    // Graticule
-    if (layer.type == "graticule") {
-      addgraticule(svg, projection, {
-        stroke: layer.stroke,
-        strokewidth: layer.strokewidth,
-        strokeopacity: layer.strokeopacity,
-        strokedasharray: layer.strokedasharray,
-        step: layer.step
+    // Outline (fill)
+    let outline = layers.find((d) => d.type == "outline");
+    if (outline) {
+      addoutline(svg, projection, {
+        fill: outline.fill,
+        stroke: "none",
+        strokewidth: "none"
       });
     }
 
-    // geojsons
-    if (layer.type == "layer") {
-      layersimple(svg, projection, layer.geojson, {
-        fill: layer.fill,
-        stroke: layer.stroke,
-        strokewidth: layer.strokewidth,
-        fillopacity: layer.fillopacity
+    // ----------------------------------------
+    layers.forEach((layer) => {
+      // Graticule
+      if (layer.type == "graticule") {
+        addgraticule(svg, projection, {
+          stroke: layer.stroke,
+          strokewidth: layer.strokewidth,
+          strokeopacity: layer.strokeopacity,
+          strokedasharray: layer.strokedasharray,
+          step: layer.step
+        });
+      }
+
+      // geojsons
+      if (layer.type == "layer") {
+        simpleLayer(svg, projection, layer.geojson, {
+          fill: layer.fill,
+          stroke: layer.stroke,
+          strokewidth: layer.strokewidth,
+          fillopacity: layer.fillopacity
+        });
+      }
+
+      // Header
+      if (layer.type == "header") {
+        addheader(svg, width, {
+          fontsize: layer.fontsize,
+          text: layer.text,
+          fill: layer.fill
+        });
+      }
+
+      // Footer
+      if (layer.type == "footer") {
+        addfooter(svg, width, height, {
+          fontsize: layer.fontsize,
+          text: layer.text,
+          fill: layer.fill
+        });
+      }
+    });
+
+    // -----------------------------------------
+
+    // Outline (stroke)
+    if (outline) {
+      addoutline(svg, projection, {
+        fill: "none",
+        stroke: outline.stroke,
+        strokewidth: outline.strokewidth
       });
     }
 
-    // Header
-    if (layer.type == "header") {
-      addheader(svg, width, {
-        fontsize: layer.fontsize,
-        text: layer.text,
-        fill: layer.fill
-      });
-    }
-  });
-
-  // Footer
-  if (layer.type == "footer") {
-    addfooter(svg, width, height, {
-      fontsize: footer.fontsize,
-      text: footer.text,
-      fill: footer.fill
-    });
+    // build
+    return Object.assign(svg.node(), {});
   }
-
-  // -----------------------------------------
-
-  // Outline (stroke)
-  if (outline) {
-    addoutline(svg, projection, {
-      fill: "none",
-      stroke: outline.stroke,
-      strokewidth: outline.strokewidth
-    });
-  }
-
-  // build
-  return Object.assign(svg.node(), {});
-}
