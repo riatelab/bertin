fd6c8db35d436c449dfb6c6c23f52133ed68690e
neocarto <nicolas.lambert@cnrs.fr>
2021-11-23 15:44:23 +0100

    tooltop for simple layers

--- src/layer-simple.js
+++ src/layer-simple.js
@@ -4,6 +4,8 @@ import * as d3geo from "d3-geo";
 import * as d3geoprojection from "d3-geo-projection";
 const d3 = Object.assign({}, d3selection, d3geo, d3geoprojection);
 import {figuration } from "./figuration.js";
+import {addtooltip } from "./tooltip.js";
+
 export function layersimple(selection, projection, features, options = {}) {
   let cols = [
     "#66c2a5",
@@ -21,9 +23,10 @@ export function layersimple(selection, projection, features, options = {}) {
   let stroke = options.stroke ? options.stroke : "white";
   let strokewidth = options.strokewidth ? options.strokewidth : 0.5;
   let fillopacity = options.fillopacity ? options.fillopacity : 1;
+  let tooltip = options.tooltip ? options.tooltip : "";
 
   // If lines
-  if (figuration(features) == "l") {
+  if (figuration(geojson) == "l") {
     stroke = options.stroke
       ? options.stroke
       : cols[Math.floor(Math.random() * cols.length)];
@@ -37,12 +40,40 @@ export function layersimple(selection, projection, features, options = {}) {
     .attr("id", "simple layer")
     .attr(":inkscape:label", "simple layer")
     .selectAll("path")
-    .data(features.features)
+    .data(geojson.features)
     .join("path")
     .attr("d", d3.geoPath(projection))
     .attr("fill", fill)
     .attr("stroke", stroke)
     .attr("stroke-width", strokewidth)
     .attr("fill-opacity", fillopacity)
-    .attr("clip-path", "url(#clip)");
+    .attr("clip-path", "url(#clip)")
+    .on("touchmove mousemove", function (event, d) {
+      if (Array.isArray(tooltip)) {
+        selection
+          .select("#info")
+          .call(
+            addtooltip,
+            `${d.properties[tooltip[0]]}\n${d.properties[tooltip[1]]}\n${
+              tooltip[2]
+            }`
+          );
+      } else {
+        selection.select("#info").call(addtooltip, `${d.properties[tooltip]}`);
+      }
+      selection
+        .select("#info")
+        .attr("transform", `translate(${d3.pointer(event, this)})`);
+      d3.select(this)
+        .attr("stroke-width", strokewidth + 0.5)
+        .attr("fill-opacity", fillopacity - 0.3)
+        .raise();
+    })
+    .on("touchend mouseleave", function () {
+      selection.select("#info").call(addtooltip, null);
+      d3.select(this)
+        .attr("stroke-width", strokewidth)
+        .attr("fill-opacity", fillopacity)
+        .lower();
+    });
 }
---
+++ src/tooltip.js
@@ -0,0 +1,44 @@
+export function addtooltip(g, value){
+  if (!value) return g.style("display", "none");
+
+  g.style("display", null)
+    .style("pointer-events", "none")
+    .style("font", "8px sans-serif");
+
+  const path = g
+    .selectAll("path")
+    .data([null])
+    .join("path")
+    .attr("fill", "#fcf7e6")
+    .attr("stroke", "#4a4d4b");
+
+  const fontweight = ["bold", "normal", "normal"];
+  const fontsize = [18, 14, 10];
+  const fontstyle = ["normal", "normal", "italic"];
+
+  const text = g
+    .selectAll("text")
+    .data([null])
+    .join("text")
+    .call((text) =>
+      text
+        .selectAll("tspan")
+        .data((value + "").split(/\n/))
+        .join("tspan")
+        .attr("x", 0)
+        .attr("y", (d, i) => `${i * 1.4}em`)
+        .style("font-weight", (_, i) => fontweight[i])
+        .style("font-size", (_, i) => fontsize[i])
+        .style("font-style", (_, i) => fontstyle[i])
+        .style("fill", "#4d4545")
+        .text((d) => d)
+    );
+
+  const { x, y, width: w, height: h } = text.node().getBBox();
+
+  text.attr("transform", `translate(${-w / 2},${15 - y})`);
+  path.attr(
+    "d",
+    `M${-w / 2 - 10},5H-5l5,-5l5,5H${w / 2 + 10}v${h + 20}h-${w + 20}z`
+  );
+}
