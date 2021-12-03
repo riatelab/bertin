e73c1918083da7670f68c229bfde7066f4cee127
neocarto <nicolas.lambert@cnrs.fr>
2021-12-03 11:47:35 +0100

    new miising layer

--- src/layer-missing.js
+++ src/layer-missing.js
@@ -2,6 +2,7 @@
 import * as d3selection from "d3-selection";
 import * as d3geo from "d3-geo";
 import * as d3geoprojection from "d3-geo-projection";
+const d3 = Object.assign({}, d3selection, d3geo, d3geoprojection);
 import {figuration } from "./figuration.js";
 import {addtooltip } from "./tooltip.js";
 
