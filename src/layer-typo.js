86206af3651cb05ce4b334951ba3ce8bd1014c57
neocarto <nicolas.lambert@cnrs.fr>
2021-12-16 14:40:16 +0100

    fix depedencies typo d3-scale-chromatic

--- src/layer-typo.js
+++ src/layer-typo.js
@@ -2,7 +2,7 @@ import * as d3selection from "d3-selection";
 import * as d3geo from "d3-geo";
 import * as d3scale from "d3-scale";
 import * as d3array from "d3-array";
-import * as d3scalechromatic from "d3-scale-chromatic;
+import * as d3scalechromatic from "d3-scale-chromatic";
 const d3 = Object.assign({}, d3selection, d3scalechromatic, d3array, d3geo, d3scale);
 
 export function layertypo(selection, projection, options = {}){
