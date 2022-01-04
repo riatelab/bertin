5efb7fb747d2d57dfb2734e499978182234288a8
neocarto <nicolas.lambert@cnrs.fr>
2022-01-04 18:37:10 +0100

    test

--- rollup.config.js
+++ rollup.config.js
@@ -8,12 +8,17 @@ import copy from 'rollup-plugin-copy';
 import del from 'rollup-plugin-delete';
 
 export default {
-    input: 'src/index.js',
-    output: {
-        format: 'umd',
-        file: 'dist/index.min.js',
-        name: 'bertin'
-    },
+
+entry: 'src/index.js',
+moduleName: 'bertin',
+  format: 'umd',
+   dest: 'build/bertin.js',
+    // input: 'src/index.js',
+    // output: {
+    //     format: 'umd',
+    //     file: 'dist/index.min.js',
+    //     name: 'bertin'
+    // },
     plugins: [
         commonjs(), // prise en charge de require
         noderesolve(), // prise en charge des modules depuis node_modules
@@ -27,25 +32,3 @@ export default {
     })
     ]
 };
-
-
-// // import de nos plugins
-// import commonjs from '@rollup/plugin-commonjs';
-// import noderesolve from '@rollup/plugin-node-resolve';
-// import babel from '@rollup/plugin-babel';
-// import { terser } from 'rollup-plugin-terser';
-//
-// export default {
-//     input: 'src/index.js',
-//     output: {
-//         format: 'umd',
-//         file: 'dist/index.min.js',
-//         name: 'bertin'
-//     },
-//     plugins: [
-//         commonjs(), // prise en charge de require
-//         noderesolve(), // prise en charge des modules depuis node_modules
-//         babel({ babelHelpers: 'bundled' }), // transpilation
-//         terser() // minification
-//     ]
-// };
