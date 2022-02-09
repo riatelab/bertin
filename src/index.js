176649310181627f1d26d1092683adf8fb750d51
neocarto <nicolas.lambert@cnrs.fr>
2022-02-09 17:08:54 +0100

    fix : dotscartogram -> dotcartogram

--- src/index.js
+++ src/index.js
@@ -3,4 +3,3 @@ export {match} from "./match.js";
 export {merge} from "./merge.js";
 export {quickdraw} from "./quickdraw.js";
 export {table2geo} from "./table2geo.js";
-export {dotscartogram} from "./layer-dotscartogram.js";
