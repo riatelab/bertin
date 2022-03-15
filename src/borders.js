d89b833dc6f010020b388e00810add8c10faa4e1
neocarto <nicolas.lambert@cnrs.fr>
2022-03-15 12:20:50 +0100

    feat: borders

--- src/borders.js
+++ src/borders.js
@@ -2,7 +2,7 @@ import * as topojsonclient from "topojson-client";
 import * as topojsonserver from "topojson-server";
 const topojson = Object.assign({}, topojsonclient, topojsonserver);
 
-export function borders({ geojson, id, values }) {
+export function borders({ geojson, id, values, type = "rel", share = null }) {
   const topo = topojson.topology({ d: geojson });
   const ids = geojson.features.map((d) => d.properties[id]);
   const neighbors = topojson.neighbors(topo.objects["d"].geometries);
@@ -18,8 +18,12 @@ export function borders({ geojson, id, values }) {
         j: ids[i],
         var_i: +valbyid.get(e),
         var_j: +valbyid.get(ids[i]),
-        rel: +valbyid.get(e) / +valbyid.get(ids[i]),
-        abs: +valbyid.get(e) - +valbyid.get(ids[i])
+        disc:
+          type == "rel"
+            ? +valbyid.get(ids[i]) != 0
+              ? +valbyid.get(e) / +valbyid.get(ids[i])
+              : null
+            : +valbyid.get(e) - +valbyid.get(ids[i])
       }
     }));
     r.forEach((e) => {
@@ -35,6 +39,19 @@ export function borders({ geojson, id, values }) {
       result.push(Object.assign(output, { geometry: geom }));
     });
   });
+
+  result.sort((a, b) => d3.descending(a.properties.disc, b.properties.disc));
+
+  const l = result.length;
+  result
+    .map((d) => d.properties)
+    .forEach((e, i) => {
+      Object.assign(e, { share: (i + 1) / l });
+    });
+
+  if (share != null) {
+    result = result.filter((d) => d.properties.share < share);
+  }
 
   return { type: "FeatureCollection", features: result };
 }
