26ba9380555de8c23c825aa6725b7cdabb5a1dad
neocarto <nicolas.lambert@cnrs.fr>
2022-06-20 13:33:45 +0200

    fix : order of colors for typologies

--- src/helpers/colorize.js
+++ src/helpers/colorize.js
@@ -4,64 +4,65 @@ import * as d3scale from "d3-scale";
 const d3 = Object.assign({}, d3scalechromatic, d3scale, d3array);
 import * as stat from "statsbreaks";
 
-export function colorize(features, input){
+export function colorize(features, input) {
   if (typeof input == "string")
     return {
-      getcol: (d) => input
+      getcol: (d) => input,
     };
 
   // choropleth
   if (typeof input == "object" && input.type == "choro") {
-  let values = input.values;
-//  let pal = input.pal ? input.pal : "Blues";
-  let colors = input.colors ?? "Blues";
-  let nbreaks = input.nbreaks ? input.nbreaks : 5;
-  let breaks = input.breaks ? input.breaks : null;
-  let k = input.k ? input.k : 1;
-  let middle = input.middle ? input.middle : false;
-  //let colors = input.colors ? input.colors : null;
-  let method = input.method ? input.method : "quantile";
-  let col_missing = input.col_missing ? input.col_missing : "#f5f5f5";
-  let txt_missing = input.txt_missing ? input.txt_missing : "No data";
-  let leg_round = input.leg_round !== undefined ? input.leg_round : undefined;
+    let values = input.values;
+    //  let pal = input.pal ? input.pal : "Blues";
+    let colors = input.colors ?? "Blues";
+    let nbreaks = input.nbreaks ? input.nbreaks : 5;
+    let breaks = input.breaks ? input.breaks : null;
+    let k = input.k ? input.k : 1;
+    let middle = input.middle ? input.middle : false;
+    //let colors = input.colors ? input.colors : null;
+    let method = input.method ? input.method : "quantile";
+    let col_missing = input.col_missing ? input.col_missing : "#f5f5f5";
+    let txt_missing = input.txt_missing ? input.txt_missing : "No data";
+    let leg_round = input.leg_round !== undefined ? input.leg_round : undefined;
 
-  if (method == "q6") {
-    nbreaks = 6;
+    if (method == "q6") {
+      nbreaks = 6;
+    }
+
+    const arr = features.map((d) => +d.properties[values]);
+    const val = arr.filter((d) => (d != undefined) & (d != null) && d != "");
+
+    const missing =
+      arr.length == val.length ? null : [txt_missing, col_missing];
+
+    if (breaks == null) {
+      breaks = stat.breaks({
+        values: val,
+        method: method,
+        nb: nbreaks,
+        k: k,
+        middle: middle,
+        precision: leg_round,
+      });
+    } else {
+      breaks = d3.sort(breaks);
+    }
+
+    if (!Array.isArray(colors)) {
+      colors = d3[`scheme${colors}`][breaks.length - 1];
+    }
+
+    let b = [...breaks];
+    b.pop();
+    b.shift();
+
+    return {
+      getcol: d3.scaleThreshold(b, colors).unknown(col_missing),
+      breaks: breaks,
+      colors: colors,
+      missing: missing,
+    };
   }
-
-  const arr = features.map((d) => +d.properties[values]);
-  const val = arr.filter((d) => (d != undefined) & (d != null) && d != "");
-
-  const missing = arr.length == val.length ? null : [txt_missing, col_missing];
-
-  if (breaks == null) {
-    breaks = stat.breaks({
-      values: val,
-      method: method,
-      nb: nbreaks,
-      k: k,
-      middle: middle,
-      precision: leg_round
-    });
-  } else {
-    breaks = d3.sort(breaks);
-  }
-
-
-  if (!Array.isArray(colors)){colors = d3[`scheme${colors}`][breaks.length - 1];}
-
-
-  let b = [...breaks];
-  b.pop();
-  b.shift();
-
-  return {
-    getcol: d3.scaleThreshold(b, colors).unknown(col_missing),
-    breaks: breaks,
-    colors: colors,
-    missing: missing
-  };
-}
 
   // typo
 
@@ -71,19 +72,32 @@ export function colorize(features, input){
     let colors = input.colors ?? "Tableau10";
     let col_missing = input.col_missing ? input.col_missing : "#f5f5f5";
     let txt_missing = input.txt_missing ? input.txt_missing : "No data";
-    const arr = Array.from(new Set(features.map((d) => d.properties[values])));
-    let types = input.types ?? arr.filter((d) => d != "" && (d != null) && (d != undefined));
 
-  if (!Array.isArray(colors)){colors = d3[`scheme${colors}`].slice(0, types.length)}else{colors.slice(0, types.length)}
+    const arr =
+      input.order ??
+      Array.from(new Set(features.map((d) => d.properties[values])));
 
+    //const arr = Array.from(new Set(features.map((d) => d.properties[values])));
+    let types =
+      input.types ?? arr.filter((d) => d != "" && d != null && d != undefined);
+
+    if (!Array.isArray(colors)) {
+      colors = d3[`scheme${colors}`].slice(0, types.length);
+    } else {
+      colors.slice(0, types.length);
+    }
 
     return {
-        getcol: d3.scaleOrdinal().domain(types).range(colors).unknown(col_missing),
-        types: types.length == arr.length ? types : [types, txt_missing].flat(),
-        colors: types.length == arr.length ? colors : [colors, col_missing].flat()
-      };
+      getcol: d3
+        .scaleOrdinal()
+        .domain(types)
+        .range(colors)
+        .unknown(col_missing),
+      types: types.length == arr.length ? types : [types, txt_missing].flat(),
+      colors:
+        types.length == arr.length ? colors : [colors, col_missing].flat(),
+    };
   }
-
 
   // split
 
@@ -109,10 +123,7 @@ export function colorize(features, input){
         arr.length == arr2.length
           ? [`>= ${split}`, `< ${split}`]
           : [[`>= ${split}`, `< ${split}`], txt_missing].flat(),
-      colors: arr.length == arr2.length ? colors : [colors, col_missing].flat()
+      colors: arr.length == arr2.length ? colors : [colors, col_missing].flat(),
     };
-
   }
-
-
 }
