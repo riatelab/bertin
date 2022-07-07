f1a4ac40d7e300c8b21f11b27d94c20787ad9195
neocarto <nicolas.lambert@cnrs.fr>
2022-07-07 13:32:19 +0200

    feat: Geojson properties selections

---
+++ src/properties.js
@@ -0,0 +1,26 @@
+import * as geo from "geotoolbox";
+
+export let properties = {
+  table: geo.table,
+  remove: function remove({ geojson, field }) {
+    return geo.remove({ x: geojson, field });
+  },
+  keep: function keep({ geojson, field }) {
+    return geo.keep({ x: geojson, field });
+  },
+  add: function add({ geojson, field, expression }) {
+    return geo.add({ x: geojson, field, expression });
+  },
+  filter: function filter({ geojson, expression }) {
+    return geo.filter({ x: geojson, expression });
+  },
+  subset: function subset({ geojson, field, selection, inverse }) {
+    return geo.subset({ x: geojson, field, selection, inverse });
+  },
+  head: function head({ geojson, field, nb }) {
+    return geo.head({ x: geojson, field, nb });
+  },
+  tail: function tail({ geojson, field, nb }) {
+    return geo.tail({ x: geojson, field, nb });
+  },
+};
