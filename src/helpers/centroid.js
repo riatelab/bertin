7e223e211b6560eca220f2f3b36f4f9723329a23
neocarto <nicolas.lambert@cnrs.fr>
2022-10-19 11:09:44 +0200

    Fix mushroom with dots (issue #76)

--- src/helpers/centroid.js
+++ src/helpers/centroid.js
@@ -2,52 +2,56 @@
 
 import { geoArea, geoCentroid, geoIdentity, geoPath } from "d3-geo";
 import { featurecollection } from "./featurecollection.js";
-
+import { figuration } from "./figuration.js";
 const d3 = Object.assign({}, { geoArea, geoCentroid, geoIdentity, geoPath });
 export function centroid(geojson, options = {}) {
-  let largest = options.largest === false ? false : true;
-  let planar = options.planar === true ? true : false;
-  let path = d3.geoPath(d3.geoIdentity());
+  if (figuration(geojson) === "p") {
+    return geojson;
+  } else {
+    let largest = options.largest === false ? false : true;
+    let planar = options.planar === true ? true : false;
+    let path = d3.geoPath(d3.geoIdentity());
 
-  geojson = featurecollection(geojson);
-  const largestPolygon = function (d) {
-    var best = {};
-    var bestArea = 0;
-    d.geometry.coordinates.forEach(function (coords) {
-      var poly = { type: "Polygon", coordinates: coords };
-      var area = planar ? path.area(poly) : d3.geoArea(poly);
-      if (area > bestArea) {
-        bestArea = area;
-        best = poly;
+    geojson = featurecollection(geojson);
+    const largestPolygon = function (d) {
+      var best = {};
+      var bestArea = 0;
+      d.geometry.coordinates.forEach(function (coords) {
+        var poly = { type: "Polygon", coordinates: coords };
+        var area = planar ? path.area(poly) : d3.geoArea(poly);
+        if (area > bestArea) {
+          bestArea = area;
+          best = poly;
+        }
+      });
+      return best;
+    };
+
+    let centers = geojson.features.map((d) => {
+      if (planar) {
+        d.geometry.coordinates = path.centroid(
+          largest == true
+            ? d.geometry.type == "Polygon"
+              ? d
+              : largestPolygon(d)
+            : d
+        );
+      } else {
+        d.geometry.coordinates = d3.geoCentroid(
+          largest == true
+            ? d.geometry.type == "Polygon"
+              ? d
+              : largestPolygon(d)
+            : d
+        );
       }
+
+      d.geometry.type = "Point";
+      return d;
     });
-    return best;
-  };
 
-  let centers = geojson.features.map((d) => {
-    if (planar) {
-      d.geometry.coordinates = path.centroid(
-        largest == true
-          ? d.geometry.type == "Polygon"
-            ? d
-            : largestPolygon(d)
-          : d
-      );
-    } else {
-      d.geometry.coordinates = d3.geoCentroid(
-        largest == true
-          ? d.geometry.type == "Polygon"
-            ? d
-            : largestPolygon(d)
-          : d
-      );
-    }
+    geojson.features = centers;
 
-    d.geometry.type = "Point";
-    return d;
-  });
-
-  geojson.features = centers;
-
-  return geojson;
+    return geojson;
+  }
 }
--- src/index.js
+++ src/index.js
@@ -25,6 +25,3 @@ export { inner } from "./layers/inner.js";
 export { waterlines } from "./layers/waterlines.js";
 export { regulardots } from "./helpers/regulardots.js";
 export { minimap } from "./layers/minimap.js";
-
-// TESTS
-// export { centroid } from "./helpers/centroid.js";
